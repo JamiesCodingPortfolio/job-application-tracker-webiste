3d4af433cf372acdad67a8169683f0446a699f48
JamiesCodingPortfolio <jamie.brown.09001@gmail.com>
2025-09-12 02:29:26 +0100

    initial commit

---
+++ server/server.js
@@ -0,0 +1,57 @@
+import dotenv from dotenv;
+import fs from fs;
+import https from https;
+import express from express;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+dotenv.config({ path: join(__dirname, '../.env') });
+
+const app = express();
+
+app.use(express.static('dist'));
+
+const HTTPS_ENABLED = process.env.HTTPS_ENABLED === 'true'
+const HTTP_PORT = parseInt(process.env.HTTP_PORT_NUMBER)
+const HTTPS_PORT = parseInt(process.env.HTTPS_PORT_NUMBER)
+const DOMAIN = process.env.DOMAIN_NAME
+
+let originPoint;
+
+if (DOMAIN === ''){
+  originPoint = "http://localhost:3000";
+}
+else{
+  originPoint = `${DOMAIN}`;
+}
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  if (req.headers.host === 'j-brown.uk') {
+    return res.redirect(`https://www.j-brown.uk${req.url}`);
+  }
+  next();
+});
+
+if (HTTPS_ENABLED){
+
+  const privateKey = fs.readFileSync(join(__dirname, '../private.key'), 'utf-8');
+  const certificate = fs.readFileSync(join(__dirname, '../certificate.crt'), 'utf-8');
+
+  const httpsServer = https.createServer(
+    { key: privateKey, cert: certificate},
+    app
+  )
+
+  httpsServer.listen(HTTPS_PORT, '0.0.0.0', () => {
+    console.log(`HTTPS Server started on port ${HTTPS_PORT}`);
+  });
+}
+
+else{
+  app.listen(HTTP_PORT, '127.0.0.1', () => {
+    console.log(`Server running on port ${HTTP_PORT}`);
+  })
+}
