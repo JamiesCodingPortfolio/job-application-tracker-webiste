22a19371a3403344d4deac5072022eb8a1e28e5c
JamiesCodingPortfolio <jamie.brown.09001@gmail.com>
2025-09-12 02:58:29 +0100

    vite setup for front-end development

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
-import dotenv from dotenv;
-import fs from fs;
-import https from https;
-import express from express;
+import dotenv from 'dotenv';
+import fs from 'fs';
+import https from 'https';
+import express from 'express';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
