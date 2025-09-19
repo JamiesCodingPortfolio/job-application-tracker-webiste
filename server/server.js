c84a322d007981a80d93e801203735f9421de9c1
JamiesCodingPortfolio <jamie.brown.09001@gmail.com>
2025-09-19 02:35:25 +0100

    fixed server.js imports

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ import dotenv from 'dotenv';
 import fs from 'fs';
 import https from 'https';
 import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
