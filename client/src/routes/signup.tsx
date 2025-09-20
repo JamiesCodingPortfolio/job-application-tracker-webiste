c4781b2e1de49149ea26befde4a5b89cddfb1453
JamiesCodingPortfolio <jamie.brown.09001@gmail.com>
2025-09-21 00:52:13 +0100

    added api call to signup

--- client/src/routes/signup.tsx
+++ client/src/routes/signup.tsx
@@ -1,4 +1,5 @@
-import { useState, FormEvent } from "react";
+import { useState, FormEvent, use } from "react";
+import { Link, useNavigate } from "react-router"
 
 const Signup = () => {
 
@@ -7,14 +8,51 @@ const Signup = () => {
   const [password, setPassword] = useState<string>('');
   const [message, setMessage] = useState<string>('');
   const [isError, setIsError] = useState<boolean>(false);
+  const navigate = useNavigate();
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     try {
+
+      const domain = import.meta.env.DOMAIN_NAME
+      ? (import.meta.env.VITE_DOMAIN_NAME as string)
+      : 'http://localhost:8080'
+
+      console.log ("Domain Name: ", domain);
+
+      const response = await fetch(`/api/signup`, {
+        method: "POST",
+        headers: {
+          "Content type": "application/json",
+        },
+        credentials: 'include',
+        mode: 'cors',
+        body: JSON.stringify({
+          email,
+          password,
+          name
+        }),
+      });
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        throw new Error(data.message || "Signup Failed");
+      }
+
+      setMessage (data.Message);
+      setIsError(false);
+      setEmail('');
+      setPassword('');
+      setName('');
+
+      navigate('/dashboard')
       
     } catch (error) {
-      
+      const message = error instanceof Error ? error.message : 'An unknown error occurred';
+      setMessage(message);
+      setIsError(true);
     }
   }
   return (
