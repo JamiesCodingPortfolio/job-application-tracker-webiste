5253293e9e3b15c67cc6247cf535f52210eb7c3b
JamiesCodingPortfolio <jamie.brown.09001@gmail.com>
2025-09-25 04:23:32 +0100

    Created signup form, css to be applied

--- client/src/routes/signup.tsx
+++ client/src/routes/signup.tsx
@@ -58,15 +58,38 @@ const Signup = () => {
   }
   return (
     <div className='h-screen w-screen flex flex-row items-center justify-center gap-10 bg-emerald-950'>
-      <form className="gap-y-2 px-8" onSubmit={handleSubmit}>
-        <label htmlFor="name" className="text-gray-600">Name</label>
+      <form className="flex flex-col items-center justify-start gap-y-2 px-8" onSubmit={handleSubmit}>
+        <label htmlFor="name" className="text-gray-400">Name</label>
         <input 
           type="text"
           id="name"
+          name="name"
           className="border rounded-lg p-2 text-center"
           placeholder="Enter your name"
           value={name}
           onChange={(e) => setName(e.target.value)}
+          required
+        />
+        <label htmlFor="email" className="text-gray-400">Email</label>
+        <input 
+          type="email"
+          id="email"
+          name="email"
+          className="border rounded-lg p-2 text-center"
+          placeholder="Enter your email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <label htmlFor="password" className="text-gray-400">Password</label>
+        <input 
+          type="password"
+          id="password"
+          name="password"
+          className="border rounded-lg p-2 text-center"
+          placeholder="Enter a password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
           required
         />
       </form>
