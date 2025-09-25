562aee65b5e5ef091b1187ebbf7715b3fbdf8305
JamiesCodingPortfolio <jamie.brown.09001@gmail.com>
2025-09-25 04:32:27 +0100

    removed link import temporarily

--- client/src/routes/signup.tsx
+++ client/src/routes/signup.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import type { FormEvent } from "react";
-import { Link, useNavigate } from "react-router"
+import { useNavigate } from "react-router"
 
 const Signup = () => {
 
