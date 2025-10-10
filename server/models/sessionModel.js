a79b37b0cfbd8d28e89aaa74c62f910c40f63c84
JamiesCodingPortfolio <jamie.brown.09001@gmail.com>
2025-10-10 01:58:45 +0100

    created Session collection model and Schema

---
+++ server/models/sessionModel.js
@@ -0,0 +1,10 @@
+import mongoose, { MongooseError } from 'mongoose'
+
+const sessionSchema = new mongoose.Schema({
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    tokenHash: { type: String, required: true},
+}, {timestamps: true});
+
+const Session = mongoose.model('Session', sessionSchema);
+
+export default Session;
--- server/server.js
+++ server/server.js
@@ -1,5 +1,3 @@
-import { Router } from 'express';
-
 import dotenv from 'dotenv';
 import fs from 'fs';
 import https from 'https';
