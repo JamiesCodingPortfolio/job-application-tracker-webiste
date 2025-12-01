ab54dee295459da4d4ae8b147d7e8dfdc508047c
JamiesCodingPortfolio <jamie.brown.09001@gmail.com>
2025-12-01 02:47:36 +0000

    Incomplete dashboard, to complete tomorrow, temporary api call implemented

--- client/src/routes/dashboard.tsx
+++ client/src/routes/dashboard.tsx
@@ -1,19 +1,24 @@
+import { useEffect } from "react";
 import { useNavigate } from "react-router";
 
 const Dashboard = () => {
 
     const navigate = useNavigate();
 
-    // const fetchDashboard = async () => {
-    //     try {
-    //         const response = await fetch('/api/applications', {
-    //             method: 'GET',
-    //             credentials: 'include'
-    //         });
-    //     } catch (error) {
+    useEffect(() => {
+      fetchDashboard();
+    })
+
+    const fetchDashboard = async () => {
+        try {
+            const response = await fetch('/api/applications', {
+                method: 'GET',
+                credentials: 'include'
+            });
+        } catch (error) {
             
-    //     }
-    // }
+        }
+    }
 
     const handleLogout = async () => {
     try {
@@ -35,19 +40,21 @@ const Dashboard = () => {
   };
 
     return (
-        <div className='h-screen w-screen flex flex-col items-center justify-center gap-10'>
-            <div className="top-header flex w-full h-10% absolute top-0 justify-center">
-                <button 
-                    onClick={handleLogout}
-                    className='absolute right-8 top bg-[#E2848C] text-white rounded-lg p-3 hover:bg-[#d8737b] transition-colors cursor-pointer shadow-md hover:shadow-lg active:scale-95'
-                >
-                    <h2>Logout</h2>
-                </button>
-            </div>
-            <h1>
-                Dashboard
-            </h1>
+      <div className='h-screen w-screen flex flex-col items-center justify-center gap-10 p-[2.5%]'>
+        <div className="top-header relative w-full flex justify-end">
+            <button 
+                onClick={handleLogout}
+                className='right-8 bg-[#E2848C] text-white rounded-lg p-3 hover:bg-[#d8737b] transition-colors cursor-pointer shadow-md hover:shadow-lg active:scale-95'
+            >
+                <h2>Logout</h2>
+            </button>
         </div>
+        <div>
+          <h1>
+              Dashboard
+          </h1>
+        </div>
+      </div>
     )
 }
 
